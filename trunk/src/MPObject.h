fb8f79135d73da4b2e9ec50c9333f7009bc79b17
vmegas@ya.ru <vmegas@ya.ru@61547564-9652-0410-91a2-2798ae2b3cd2>
2008-07-22 06:51:10 +0000

    Update MPFeature and MPObject

--- trunk/src/MPObject.h
+++ trunk/src/MPObject.h
@@ -1,25 +1,15 @@
 #import <Foundation/Foundation.h>
 #import <MPFeature.h>
 
-@protocol MPPositon
-	// xyz
-@end
-
-@protocol MPOrientation
-	// rx, ry, rz (quaternion)
-@end
-
-@protocol MPSize
-	// sz, sy, sz
-@end
-
-@protocol MPBaseObject < MPPositon, MPOrientation, MPSize >
+@interface MPBaseObject : NSObject
 	// etc...
 @end
 
-@interface MPObject : NSObject < MPBaseObject/*NSCopying, NSMutableCopying, NSCoding*/ > 
+// i think this class will be always mutable (nekro)
+@interface MPObject : MPBaseObject /*< NSCopying, NSMutableCopying, NSCoding > */
 	// representation in world ( id<MPBaseObject> repr, access functions )
 	// composite stuff (iterators, children managment, owner managment)
+		///!!! is NSMutableArray NSutableCopying???
 	// NSCopying, MutableCopying
 	// NSCoding
 	// Features stuff ( add/remove, (MPFeature*)getFeatureBy[Id/Name], (NSSet) getFeatures, isA(feature[id/name]) )
