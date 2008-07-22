1d2156fffb141565a328fb6fc78ad24318aec45c
vmegas@ya.ru <vmegas@ya.ru@61547564-9652-0410-91a2-2798ae2b3cd2>
2008-07-22 06:51:10 +0000

    Update MPFeature and MPObject

--- src/MPFeature.h
+++ src/MPFeature.h
@@ -3,11 +3,23 @@
 typedef unsigned long feature_id_t;
 #define NOT_FEATURE ( (feature_id_t)(-1) )
 
-@protocol MPFeature /*< NSCopying >*/
-- (NSString *) name;
+@protocol MPFeatureData /*< NSCopying, NSCoding >*/
+// it's just a dummmy now
+@end
 
+@protocol MPFeature /*< NSCopying, NSCoding >*/
+// returns array of objects with this feature
+- (NSArray *) getObjects;
+// in @intefrace also addObject or something similiar needs
+// information
+/// name
+- (NSString *) name;
+///type
 - (NSString *) typeName;
 - (feature_id_t) typeId;
+
+// feature data access
+- (id <MPFeatureData>) data; // dictionary + struct + sync
 
 // something another...
 @end
