7ad8f4484c5b94cc8a3825b65d31be2af0d210eb
ikosoft@ya.ru <ikosoft@ya.ru@61547564-9652-0410-91a2-2798ae2b3cd2>
2008-07-27 16:08:20 +0000

    dictionary update: added dict_clear and dict_copy functions

--- src/dictionary.h
+++ src/dictionary.h
@@ -7,8 +7,10 @@ typedef struct
 } dictionary_node;
 
 dictionary_node *dict_getempty();
+dictionary_node *dict_copy(dictionary_node *source);
 
 int dict_find(dictionary_node *tree, char *key, char *valuebuf);
 void dict_insert(dictionary_node *tree, char *key, char *value);
 void dict_remove(dictionary_node *tree, char *key);
+void dict_clear(dictionary_node *tree);
 
