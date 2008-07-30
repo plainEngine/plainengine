d4b68ff0f5b12d24c6e4f2ad5215379970df7507
ikosoft@ya.ru <ikosoft@ya.ru@61547564-9652-0410-91a2-2798ae2b3cd2>
2008-07-30 18:07:36 +0000

    dictionary updated and documented

--- trunk/src/createdoc.sh
+++ trunk/src/createdoc.sh
@@ -1,2 +1,2 @@
-rm doc/*
-autogsdoc MPCodeTimer.h MPDictionary.h MPConfigDictionary.h MPFileLogChannel.h MPLog.h MPNotificationQueue.h common.h common_defines.h MPNotifications.h -MakeFrames YES -DocumentationDirectory ../docs/ -Project plainEngine
+rm ../docs/*
+autogsdoc *.h -MakeFrames YES -DocumentationDirectory ../docs/ -Project plainEngine 2> /dev/null >> /dev/null
